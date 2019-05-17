67842fc0c6ff2bd180ed15ce9128d3f8816a6679
Nicolas Witczak <nicolaswitczak@MacBook-Pro-de-Nicolas.local>
2019-05-17 21:19:03 +0200

    One Touchbar button working

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,44 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "dev-touch-bar" is now active!');
+	function registerCommand( cmdid : string , cb: (...arg:any[] ) => any )
+	{
+		let disposable = vscode.commands.registerCommand( cmdid , cb );
+		context.subscriptions.push(disposable);
+	}
+	
+	function registerTextCommand( cmdid : string ,callback: (textEditor: vscode.TextEditor, ...args: any[]) => void )
+	{
+		let disposable = vscode.commands.registerTextEditorCommand( cmdid , callback );
+		context.subscriptions.push(disposable);
+	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	function registerInsertKey(cmdid : string , insertkey: string)
+	{
+		registerTextCommand( cmdid , (textEditor: vscode.TextEditor) => 
+		{
+			textEditor.edit( (editBuilder:vscode.TextEditorEdit) => 
+			{
+				editBuilder.insert( textEditor.selection.active , insertkey ) ;
+			});
+		} );
+	}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	console.log('Congratulations, your extension "dev-touch-bar" is now active!');
+	registerCommand('extension.helloWorld', () => vscode.window.showInformationMessage('Hello World!'));
+	registerInsertKey('extension.braceOpen','{');
+	registerInsertKey('extension.braceClose','}');
+	registerInsertKey('extension.bracketOpen','[');
+	registerInsertKey('extension.bracketClose',']');
+	registerInsertKey('extension.pipe','|');
+	registerInsertKey('extension.tilde','] ');
 }
 
 // this method is called when your extension is deactivated
