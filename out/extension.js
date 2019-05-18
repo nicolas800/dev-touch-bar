e9a0e84aea2e0e7f4348a9464a0a9f3fe35a055f
Nicolas Witczak <nicolaswitczak@MacBook-Pro-de-Nicolas.local>
2019-05-18 12:16:51 +0200

    Code cleanup

--- out/extension.js
+++ out/extension.js
@@ -1,10 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
     function registerCommand(cmdid, cb) {
         let disposable = vscode.commands.registerCommand(cmdid, cb);
@@ -21,8 +17,6 @@ function activate(context) {
             });
         });
     }
-    console.log('Congratulations, your extension "dev-touch-bar" is now active!');
-    registerCommand('extension.helloWorld', () => vscode.window.showInformationMessage('Hello World!'));
     registerInsertKey('extension.braceOpen', '{');
     registerInsertKey('extension.braceClose', '}');
     registerInsertKey('extension.bracketOpen', '[');
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-
-
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
+export function activate(context: vscode.ExtensionContext) 
+{
 	function registerCommand( cmdid : string , cb: (...arg:any[] ) => any )
 	{
 		let disposable = vscode.commands.registerCommand( cmdid , cb );
@@ -32,8 +25,6 @@ export function activate(context: vscode.ExtensionContext) {
 		} );
 	}
 
-	console.log('Congratulations, your extension "dev-touch-bar" is now active!');
-	registerCommand('extension.helloWorld', () => vscode.window.showInformationMessage('Hello World!'));
 	registerInsertKey('extension.braceOpen','{');
 	registerInsertKey('extension.braceClose','}');
 	registerInsertKey('extension.bracketOpen','[');
